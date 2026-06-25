2f5c77f14b47e083db8564441daa46caec1e71be
deekshithaanyaboina22-alt <deekshithaanyaboina22@gmail.com>
2026-06-25 23:51:52 +0530

    Update frontend API URL for production

--- frontend/vite-project/src/services/api.js
+++ frontend/vite-project/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://utpatti-project-management-utility-1.onrender.com",
 });
 
 export default api;
